9af2f73eade6e133e8fae73da4ad835bf21e06e0
busyluo <2488091@qq.com>
2016-03-28 21:00:57 +0800

    Initial Commit

---
+++ BounceAnimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BounceAnimate
+//
+//  Created by Xavier on 3/28/16.
+//  Copyright © 2016 Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BounceAnimate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BounceAnimate
+//
+//  Created by Xavier on 3/28/16.
+//  Copyright © 2016 Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
